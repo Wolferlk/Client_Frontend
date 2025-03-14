9daf72b83f9b024d4e659d78633030b56978cf9a
wolferlk <sasiofficial25@gmail.com>
2025-03-14 16:26:05 +0530

    navbarupdate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ export default function Navbar() {
               transition={{ delay: 0.2, duration: 0.5 }}
             >
               <span className="text-2xl font-bold text-white">
-                <span className="text-purple-500">G</span>AMAGE <span className="text-purple-500">R</span>ECRUITERS
+                GAMAGE RECRUITERS
               </span>
             </motion.div>
           </motion.div>
